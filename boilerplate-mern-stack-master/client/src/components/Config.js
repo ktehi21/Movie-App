6b622b44185554136696c2f682cfeea10f665279
ktehi21 <k.tehi21@gmail.com>
2023-10-23 23:53:52 -0700

    movieDB api basic setting

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -4,4 +4,6 @@ export const USER_SERVER = '/api/users';
 // Handling Duplicate URLs
 export const API_URL = 'https://api.themoviedb.org/3/';
 
+export const API_KEY = process.env.TMDB_API_KEY;
+console.log("콘피그 파일 키 확인중",process.env.TMDB_API_KEY);
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
