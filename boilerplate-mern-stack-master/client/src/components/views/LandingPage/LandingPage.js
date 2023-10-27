b31c864daeb393fe16faab951536fbd10100f2ec
ktehi21 <k.tehi21@gmail.com>
2023-10-26 23:51:07 -0700

    add header and 'Load More' button

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY } from '../../Config';
 
 function LandingPage() {
+// console.log("API_KEY", API_KEY);
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/550?api_key=2e5e5cdf00ad11c28d90ca8359faefe4&language=en-US&page=1`;
+        
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => console.log(response))
+
+        
+    }, [])
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0' }}>
+            {/* Main Image */}
+
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+
+                <h2>Movies by latest</h2>
+                <hr />
+
+                {/* Movie Grid Cards */}
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button> Load More </button>
+            </div>
+
+        </div>
     )
 }
 
